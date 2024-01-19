eaef73e14236b21c0bd47f896f2884a5e72f8bb5
Neloy Barman <neloycareer018@gmail.com>
2024-01-19 13:36:02 +0600

    Deployment updated

--- deployment/app.py
+++ deployment/app.py
@@ -28,6 +28,111 @@ pytorch_model = model.eval()
 
 labels = ['Ayre', 'Catla', 'Chital', 'Ilish', 'Kachki', 'Kajoli', 'Koi', 'Magur', 'Mola Dhela', 'Mrigal', 'Pabda', 'Pangash', 'Poa', 'Puti', 'Rui', 'Shing', 'Silver Carp', 'Taki', 'Telapia', 'Tengra']
 
+
+found_and_test = {
+     'Ayre' : {
+        'found': 'Found in South and Southeast Asia',
+        'taste': 'Sweet taste',
+        'scientific name': 'Sperata aor',
+    },
+     'Catla' : {
+        'found': 'Native to South Asia',
+        'taste': 'sweet and nutty flavors',
+        'scientific name': 'Catla catla',
+    },
+     'Chital' : {
+        'found': 'Native to South and Southeast Asia',
+        'taste': 'sweet taste',
+        'scientific name': 'Chitala chitala',
+    },
+     'Ilish' : {
+        'found': 'Found in South Asia',
+        'taste': 'sweet and salty taste',
+        'scientific name': 'Tenualosa ilisha',
+    },
+     'Kachki' : {
+        'found': 'Found in South and Southeast Asia',
+        'taste': 'great when fresh',
+        'scientific name': 'Corica soborna Hamilo',
+    },
+     'Kajoli' : {
+        'found': 'Native to Asia',
+        'taste': 'delicate and tastes divine',
+        'scientific name': 'Ailia coila',
+    },
+     'Koi' : {
+        'found': 'Native to Asia and Europe',
+        'taste': 'mild flavor and firm flesh that tastes slightly sweet',
+        'scientific name': 'Cyprinus rubrofuscus',
+    },
+     'Magur' : {
+        'found': 'Native to Southeast Asia',
+        'taste': 'delicate flavor',
+        'scientific name': 'Clarias batrachus',
+    },
+     'Mola Dhela' : {
+        'found': 'Found in South Asia',
+        'taste': 'black cod or lobster',
+        'scientific name': 'Osteobrama cotio',
+    },
+     'Mrigal' : {
+        'found': 'Native to the Indo-Gangetic riverine systems of South Asia.',
+        'taste': 'sweet, mild taste',
+        'scientific name': 'Cirrhinus cirrhosus',
+    },
+     'Pabda' : {
+        'found': 'freshwater',
+        'taste': 'mild taste',
+        'scientific name': 'Ompok bimaculatus',
+    },
+     'Pangash' : {
+        'found': 'Native to South and Southeast Asia',
+        'taste': 'Mild, sweet, and moist taste',
+        'scientific name': 'Pangasius pangasius',
+    },
+     'Poa' : {
+        'found': 'Found in South Asia',
+        'taste': 'Mild and flaky to bold and robust',
+        'scientific name': 'Otolithoides pama',
+    },
+     'Puti' : {
+        'found': 'native to Asia',
+        'taste': 'does not taste so good but bitter',
+        'scientific name': 'Puntius sophore',
+    },
+     'Rui' : {
+        'found': 'Found in South Asia',
+        'taste': 'almost free of a "fishy" taste',
+        'scientific name': 'Labeo rohita',
+    },
+     'Shing' : {
+        'found': 'native to South Asia',
+        'taste': 'sweet, mild taste',
+        'scientific name': 'Heteropneustes fossilis',
+    },
+     'Silver Carp' : {
+        'found': 'native to eastern Asia',
+        'taste': 'similar in flavor and texture to tilapia and catfish',
+        'scientific name': 'Hypophthalmichthys molitrix',
+    },
+     'Taki' : {
+        'found': 'found in South and Southeast Asia',
+        'taste': 'mild flavor',
+        'scientific name': 'Channa striata',
+    },
+     'Telapia' : {
+        'found': 'native to Africa and the Levant',
+        'taste': 'sweet, mild taste',
+        'scientific name': 'Oreochromis niloticus',
+    },
+     'Tengra' : {
+        'found': 'found in South and Southeast Asia',
+        'taste': 'delicate, slightly sweet taste',
+        'scientific name': 'Mystus tengara',
+    },
+}
+
+
 def xai_visualization(image, image_tensor, targeted_category, model, target_layers):
 
   cam = GradCAM(model = model, target_layers = target_layers)
@@ -93,7 +198,27 @@ def classify_image(image_path):
   # print(f"Category with most probability: {np.argmax(probs)}")
   xai_image = "xai/xai_visualization.png"
 
-  return xai_image, dict(zip(labels, map(float, probs)))
+  # return xai_image, dict(zip(labels, map(float, probs)))
+
+  predicted_category = labels[targeted_category]
+
+  information = ""
+
+  if predicted_category in found_and_test.keys():
+    information = found_and_test[predicted_category]
+
+  # return image_path, information, dict(zip(labels, map(float, probs)))
+  # print(information)
+    
+  information = f'''
+    Scientific Name: {information['scientific name']};\n 
+    Taste: {information['taste'].capitalize()};\n 
+    Availability: {information['found'].capitalize()}
+  '''
+
+  # print(information)
+
+  return xai_image, str(information), dict(zip(labels, map(float, probs)))
 
 # classify_image('test images/unknown_01.jpg')
 
@@ -106,6 +231,9 @@ outputs = [
     gr.Image(
       label = "GradCAM visualization", 
       show_label = True
+    ),
+    gr.Label(
+      label = "Information"
     ),
     gr.Label(
       num_top_classes=5, 
